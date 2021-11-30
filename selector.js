b0e84648f4e84ff108a177f08b379ee3fa428a60
V1ll4a <92698844+V1ll4a@users.noreply.github.com>
2021-11-30 15:11:07 -0500

    Add files via upload

---
+++ selector.js
@@ -0,0 +1,12 @@
+nombre=prmpt("Diga su nombre")
+apellido=prompt("Diga su apellido")
+edad=prompt("Diga su edad")
+edad10=parseInt(edad)+10
+console.log("Hola"+nombre+apellido)
+var n =
+Number(edad)+"<br>"
+   if(edad!==Number){
+       console.log("Error")
+   }else{
+       console.log("Dentro de 10 años usted tendra"+edad10)
+   }
